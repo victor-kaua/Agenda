a82b2ae16e916db743fce605b63330b093d9df37
victor-kaua <victorkauamartinsnun@gmail.com>
2022-04-13 16:45:16 -0300

    VASCO

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(verbose_name='Data de Criação', auto_now=True)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
